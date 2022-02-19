5b2688fd9bd94381ff8b68307346d369b70dc4e7
Muhammad Alwi <alwi3479@gmail.com>
2022-02-19 12:58:13 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In magnam fuga quia asperiores harum error quaerat cum culpa. Perspiciatis atque nobis deserunt quae et accusamus id expedita, culpa nihil fugit.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
