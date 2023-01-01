429f7733d6aba50bc78bf3ba2fda9aad9cd97db3
webprogrammingptikb21 <alwi3479@gmail.com>
2023-01-01 21:08:08 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum et nostrum corrupti, similique nesciunt asperiores cum dolorem harum quasi eum vel voluptas consequuntur maxime est alias adipisci cumque temporibus blanditiis.</p>
 </body>
 </html>
